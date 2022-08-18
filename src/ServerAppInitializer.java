1d86e7da2dcd9462940a74c7c59b5ef98319c0ec
MilashaThathsarani <milashathathsarni99@gmail.com>
2022-08-18 11:03:49 +0530

    Updated

--- src/ServerAppInitializer.java
+++ src/ServerAppInitializer.java
@@ -1,5 +1,11 @@
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
 
 public class ServerAppInitializer extends Application {
 
@@ -8,7 +14,12 @@ public class ServerAppInitializer extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) {
-
+    public void start(Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("view/LogIn.fxml"));
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.initStyle(StageStyle.UNDECORATED);
+        //enableMove(scene, primaryStage);
+        primaryStage.show();
     }
 }
