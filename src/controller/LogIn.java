22399398d03eb256c0a66695ee209d1823c96a46
MilashaThathsarani <milashathathsarni99@gmail.com>
2022-08-18 10:19:19 +0530

    Updated

--- src/controller/LogIn.java
+++ src/controller/LogIn.java
@@ -4,36 +4,36 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
 import java.io.IOException;
 import java.net.URL;
 
-public class LogInController {
-    public TextField userNametxt;
+public class LogIn {
     public Button btnLogin;
-
-    public void userNameOnAction(ActionEvent actionEvent) {
-    }
-
-    public void loginOnAction(ActionEvent actionEvent) throws IOException {
-       loadChat();
-    }
+    public TextField userNametxt;
 
     private void loadChat() throws IOException {
         Stage exitstage = (Stage) btnLogin.getScene().getWindow();
         exitstage.close();
-        URL resource = this.getClass().getResource("/view/ChatPage.fxml");
+        URL resource = this.getClass().getResource("view/ChatPage.fxml");
         Parent load = FXMLLoader.load(resource);
         Scene scene = new Scene(load);
         Stage stage = new Stage();
         stage.initStyle(StageStyle.UNDECORATED);
+        //stage.getIcons().add(new Image("assets/livechatLogo.png"));
         stage.setScene(scene);
         //enableMove(scene,stage);
         stage.show();
+    }
+
+    public void loginOnAction(ActionEvent actionEvent) throws IOException {
+        loadChat();
     }
 }
